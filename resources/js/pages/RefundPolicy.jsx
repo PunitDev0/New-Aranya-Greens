50804c3f522cd59d463ee2015186c6d42df039b1
PunitDev0 <punitdeveloper1@gmail.com>
2025-10-27 17:05:20 +0530

    update build

--- resources/js/pages/RefundPolicy.jsx
+++ resources/js/pages/RefundPolicy.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RefundPolicy = () => {
   return (
-    <section className="py-10 bg-white text-black h-screen">
+    <section className="py-10 bg-white text-black min-h-screen">
       <div className="container mx-auto w-4/5 text-justify">
         <div className="flex flex-col">
           <div>
@@ -10,18 +10,32 @@ const RefundPolicy = () => {
               Cancellation & Refund Policy
             </h2>
 
-            <p className="text-base font-light leading-6 mb-4">
+            <p className="text-base font-light leading-7 mb-4">
               <strong>
-                YP Infra Pvt. Ltd. believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy:
-                <br />
-                All unsuccessful applications will be refunded registration amount within 7 Working Days of the Allotment Date
-                <br />
-                Refunds will be processed on the same account that which user has provided us.
-                <br />
-                There is 100% Refund policy with no charges if Allotment is not confirmed.
-                <br />
-                Please contact info@deendayaljanawasyojnareg.com for Cancellation and More information.
+                FLOW TECHESTATE believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy:
               </strong>
+            </p>
+
+            <ul className="list-disc list-inside text-base font-light leading-7 mb-4">
+              <li>
+                All unsuccessful applications will be refunded registration amount within <strong>14 Working Days</strong> of the Allotment Date.
+              </li>
+              <li>
+                Refunds will be processed on the same account that the user has provided us.
+              </li>
+              <li>
+                There is a <strong>100% Refund policy</strong> with no charges if Allotment is not confirmed.
+              </li>
+            </ul>
+
+            <p className="text-base font-light leading-7">
+              For Cancellation and more information, please contact{' '}
+              <a
+                href="mailto:info@deendayaljanawasyojnareg.com"
+                className="text-blue-600 hover:underline"
+              >
+                info@deendayaljanawasyojnareg.com
+              </a>.
             </p>
           </div>
         </div>
